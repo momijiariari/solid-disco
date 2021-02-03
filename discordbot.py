e527c88215037fd10231ff473a8f6e512c050add
momijiariari <78310866+momijiariari@users.noreply.github.com>
2021-02-04 07:40:33 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -16,6 +16,14 @@ async def on_command_error(ctx, error):
 @bot.command()
 async def neko(ctx):
     await ctx.send('にゃーん')
+    
+@bot.command()
+async def molcar(ctx):
+    await ctx.send('pui pui')
+    
+@bot.command()
+async def ping(ctx):
+    await ctx.send('pong')
 
 
 bot.run(token)
