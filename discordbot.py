27b0e710c266fb2e68cd971ec7614a58c419b0b7
momijiariari <78310866+momijiariari@users.noreply.github.com>
2021-02-04 21:42:55 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,7 +1,7 @@
 from discord.ext import commands
 import os
 import traceback
-#import random
+import random
 
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
@@ -22,6 +22,8 @@ async def molcar(ctx):
 
 @bot.command()
 async def ping(ctx):
-    await ctx.send('pong')
+    random_content=['magnam', 'magnam', 'magnam', "ふわふわ"]
+    content = random.choice(random_contents)
+    await ctx.send(content)
 
 bot.run(token)
