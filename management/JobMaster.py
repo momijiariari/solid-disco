986ee86bfe4cfd18307bdc361324806ccb3a3c49
momijiariari <78310866+momijiariari@users.noreply.github.com>
2021-02-07 22:05:32 +0900

    Update JobMaster.py

--- management/JobMaster.py
+++ management/JobMaster.py
@@ -12,7 +12,6 @@ class JobMaster():
     def __init__(self, job_names):
         self.joblist = [Villager(), Werewolf(), Seer(), Thief(), Madman(), Suicider(), Topvillager(), Wolfking()]
         self.jobdict = {job.getName():job for job in self.joblist}
-
         self.jobs = [self.jobdict[jobname] for jobname in job_names]
         self.jobs_display = ' '.join([job.getDisplayName() for job in self.jobs])
 
