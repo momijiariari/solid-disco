ff37cbe28f5eb22d94f4ad7f310b900151bf6955
momijiariari <78310866+momijiariari@users.noreply.github.com>
2021-02-11 22:26:21 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -7,29 +7,42 @@ bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
 @bot.event
-async def on_command_error(ctx, error):
+async def on_command_error(self, error):
     orig_error = getattr(error, "original", error)
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
+    await self.send(error_msg)
+    
+@bot.event
+async def on_message(self, message):
+    s = message.content.split(' ')
+    author = message.author
+    try:
+        if not s[0] in self.forbidden_commands_per_phase[self.phase]:
+            gen = self.commands[s[0]](s[1:], author)
+            if gen is not None:
+                for mem, mes in gen:
+                    await self.send_message(self.createDest(mem), mes)
+    except KeyError:
+        pass
 
 @bot.command()
-async def neko(ctx):
-    await ctx.send('にゃーん')
+async def neko(self):
+    await self.send('にゃーん')
 
 @bot.command()
-async def Neko(ctx, *args):
+async def Neko(self, *args):
     for arg in args:
         argnya = arg + 'にゃーん'
-        await ctx.send(argnya)
+        await self.send(argnya)
 
 @bot.command()
-async def molcar(ctx):
-    await ctx.send('pui pui')
+async def molcar(self):
+    await self.send('pui pui')
 
 @bot.command()
-async def ping(ctx):
+async def ping(self):
     random_contents = ['magnam', 'magnam', 'magnam', 'magnam', 'ふわふわ', 'pong', 'pong']
     content = random.choice(random_contents)
-    await ctx.send(content)
+    await self.send(content)
 
 bot.run(token)
