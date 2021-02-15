71266d3185fd589b091181d48dcffb77a22af791
momijiariari <78310866+momijiariari@users.noreply.github.com>
2021-02-15 09:38:42 +0900

    Update gameboard.py

--- gameboard.py
+++ gameboard.py
@@ -1,24 +1,16 @@
 import random
 
-class GameMaster():
+class Gameboard():
 
-    def __init__(self, players, jobs):
-        # 役割の配布
-        for i, player in enumerate(players):
-            random.shuffle(jobs)
-            player.Ibecome(jobs.pop(-1))
-
-        self.players = players
-        self.jobs = jobs
-
-        # for seer
-        self.nouse_display = ' '.join([
-            job.getDisplayName() for job in self.jobs if not job.usingSomeone()
-        ])
-
-        # for werewolf
-        self.werewolfs_list = [player for player in self.players if player.Iam().amIWerewolf()]
-
-        # for thief
-        self.thief_exchange_actor = None
-        self.thief_exchange_target = None
+    def __init__(self, num):
+        Repair = num
+        Bom = 1
+        Silent = 4*num - 1
+        self.card = (Repair, Bom, Silent)
+        self.unrepair = Repair
+        
+    def discard(self, card):
+        self.card[card] -= 1
+        
+    def repair(self):
+        self.unrepair -= 1
