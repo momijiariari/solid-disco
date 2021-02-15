8b4310ccde38f27c7e8963bf799384640e426c6d
momijiariari <78310866+momijiariari@users.noreply.github.com>
2021-02-15 09:40:06 +0900

    Update gameboard.py

--- gameboard.py
+++ gameboard.py
@@ -5,12 +5,12 @@ class Gameboard():
     def __init__(self, num):
         Repair = num
         Bom = 1
-        Silent = 4*num - 1
+        Silent = 4 * num - 1
         self.card = (Repair, Bom, Silent)
         self.unrepair = Repair
-        
+
     def discard(self, card):
         self.card[card] -= 1
-        
+
     def repair(self):
         self.unrepair -= 1
