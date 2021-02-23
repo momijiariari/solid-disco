6c8f406f2701e0bda3fe83cbc9f90900a0ba78fd
momijiariari <78310866+momijiariari@users.noreply.github.com>
2021-02-23 15:29:38 +0900

    Update run.py

--- run.py
+++ run.py
@@ -1,10 +1,10 @@
 from dowClient import dowClient
 import os
 
-token = os.environ['DISCORD_BOT_TOKEN']
+# token = os.environ['DISCORD_BOT_TOKEN']
 client = dowClient()
 
 with open('./key', 'r') as f:
     key = f.read()
 
-client.run(token)
+# client.run(token)
